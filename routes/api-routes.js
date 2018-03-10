f73ced971c11e91278a1698f155b264a7536fc6b
timster2938 <ruggt001@icloud.com>
2018-03-10 10:34:35 -0500

    Models

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,12 +2,10 @@ var db = require("../models");
 
 // routes needed: 
 // getting new user and inserting into database
-User.create({ username: 'barfooz', isAdmin: true }, { fields: [ 'username' ] }).then(user => {
-  // let's assume the default of isAdmin is false:
-  console.log(user.get({
-    plain: true
-  })) // => { username: 'barfooz', isAdmin: false }
-});
+// db.Users.create({ user_name: 'Timster', user_id: 'demo@mail.com', user_password: 'password'}).then(user => {
+//   // let's assume the default of isAdmin is false:
+//   console.log(user.data);
+// });
 // logging in old user (checking databse to see if there's a username match)
 // showing username and points earned
 // getting new drawing stored
