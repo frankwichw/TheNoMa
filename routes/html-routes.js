8a83dfa6392f2db7ed3bc41d65847fc1b183bd76
frankwichw <frankwichw@gmail.com>
2018-03-13 19:23:55 -0400

    user name rendered

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,11 +7,12 @@ module.exports = function(app) {
   });
 
   app.get("/welcome", function(req, res) {
-   res.send("Hello! Your ID is: " + req.user.id + "\nYour name is: " + req.user.user_name);
-    // res.render("welcome");
+  //  res.send("Hello! Your ID is: " + req.user.id + "\nYour name is: " + req.user.user_name);
+    console.log(req.user.user_name);
+    var userName = req.user.user_name;
+    res.render("welcome", {username: userName});
   });
 
-  // blog route loads blog.html
   app.get("/draw", function(req, res) {
     res.render("draw");
   });
