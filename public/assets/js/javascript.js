8dfae9a428c645e41e5879dae555a996cd5c97c5
frankwichw <frankwichw@gmail.com>
2018-03-16 16:14:49 -0400

    guess api routes fix

--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -1,90 +1,91 @@
-$('select').material_select();
+$(document).ready(function(){
+    $('select').material_select();
 
-// Random Drawing Prompt
+    // Random Drawing Prompt
 
-var drawThis = ["Taco cat", "Don't eat the cookies", "Pretending to read", "Candyman", "Eggmobile", "Lonely Plumber", ""];
-
-
-var guessNumber = Math.floor(Math.random() * 5);
-console.log(guessNumber);
-
-var subject = drawThis[guessNumber];
-console.log("subject" + subject);
-
-$('#user-to-draw').append("<h3>" + subject + "</h3>");    
+    var drawThis = ["Taco cat", "Don't eat the cookies", "Pretending to read", "Candyman", "Eggmobile", "Lonely Plumber", ""];
 
 
-$("#save").on("click", function (event) {
+    var guessNumber = Math.floor(Math.random() * 5);
+    console.log(guessNumber);
+
+    var subject = drawThis[guessNumber];
+    console.log("subject" + subject);
+
+    $('#user-to-draw').append("<h3>" + subject + "</h3>");    
+
+
+    $("#save").on("click", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        var data = canvas.toDataURL();
+        //new drawing object
+        var drawingInput = {
+            drawing_descriptor: subject,
+            UserId: 3,
+            drawing: data
+        };
+        
+        console.log(drawingInput);
+        // Send the POST request.
+        $.ajax("/api/drawing", {
+            type: "POST",
+            data: drawingInput
+        }).then(
+            function () {
+                console.log("created new drawing");
+                // Reload the page to get the updated list
+                location.replace("/welcome");
+            }
+        );
+    }); 
+
+    var newUserGuess = '';
+
+    $("#guess-submit").on("click", function (event) {
+    event.preventDefault();
+    newUserGuess = $("#guess").val().trim();
+    $("#rate-art").removeClass("hide");
+    $("#guess-form").addClass("hide");
+    });
+
+    $("#submit-all").on("click", function (event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
+    //    var newUserGuess = $("#guess").val().trim();
+    var drawingID = $("#random-guessImg").data("attribute");
+    console.log(newUserGuess);   
+    console.log("drawing id: " + drawingID);
 
-    var data = canvas.toDataURL();
-    //new drawing object
-    var drawingInput = {
-        drawing_descriptor: subject,
-        UserId: 3,
-        drawing: data
+    var userRating = parseInt($("#art-rating").val());
+    //new guess object
+    var userScore = userRating * 10;
+
+    var guessInput = {
+        guess: newUserGuess,
+        DrawingId: drawingID,
+        rating: userRating
     };
     
-    console.log(drawingInput);
     // Send the POST request.
-    $.ajax("/api/drawing", {
+    $.ajax("/api/guess", {
         type: "POST",
-        data: drawingInput
+        data: guessInput
     }).then(
         function () {
-            console.log("created new drawing");
-            // Reload the page to get the updated list
-            location.replace("/welcome");
+            console.log("created new guess");
         }
     );
-}); 
 
-var newUserGuess = '';
-
-$("#guess-submit").on("click", function (event) {
-   event.preventDefault();
-   newUserGuess = $("#guess").val().trim();
-   $("#rate-art").removeClass("hide");
-   $("#guess-form").addClass("hide");
-});
-
-$("#submit-all").on("click", function (event) {
-   // Make sure to preventDefault on a submit event.
-   event.preventDefault();
-   var newUserGuess = $("#guess").val().trim();
-
-   var userRating = parseInt($("#art-rating").val());
-   //new guess object
-   var userScore = userRating * 10;
-
-   var guessInput = {
-       guess: newUserGuess,
-       DrawingId: 3,
-       userId: 2,
-       rating: userRating
-   };
-
-   console.log(guessInput);
-   // Send the POST request.
-   $.ajax("/api/guess", {
-       type: "POST",
-       data: guessInput
-   }).then(
-       function () {
-           console.log("created new guess");
-           // Reload the page to get the updated list
-           // location.reload();
-       }
-   );
-
-   $.ajax("/api/score", {
-       type: "PUT",
-       data: userScore
-   }).then(
-       function () {
-           console.log("user score updated");
-           // Reload the page to get the updated list
-           location.replace("/welcome");
-       });
+    $.ajax("/api/score", {
+        type: "PUT",
+        data: userScore
+    }).then(
+        function () {
+            console.log("user score updated");
+            // Reload the page to get the updated list
+            // location.replace("/welcome");
+        });
+    });
 });
