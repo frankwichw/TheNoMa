c35ee67df05e416c4112bb31646356d40664ac86
timster2938 <ruggt001@icloud.com>
2018-03-18 00:39:19 -0400

    user id and user score udpates

--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -1,10 +1,9 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('select').material_select();
 
     // Random Drawing Prompt
 
     var drawThis = ["Taco cat", "Don't eat the cookies", "Pretending to read", "Candyman", "Eggmobile", "Lonely Plumber", ""];
-
 
     var guessNumber = Math.floor(Math.random() * 5);
     console.log(guessNumber);
@@ -12,21 +11,23 @@ $(document).ready(function(){
     var subject = drawThis[guessNumber];
     console.log("subject" + subject);
 
-    $('#user-to-draw').append("<h3>" + subject + "</h3>");    
+    $('#user-to-draw').append("<h3>" + subject + "</h3>");
 
 
     $("#save").on("click", function (event) {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
 
+        // ensure id value is used and is an integer
+        var userID = parseInt($("#userID").val());
         var data = canvas.toDataURL();
         //new drawing object
         var drawingInput = {
             drawing_descriptor: subject,
-            UserId: 3,
+            UserId: userID,
             drawing: data
         };
-        
+
         console.log(drawingInput);
         // Send the POST request.
         $.ajax("/api/drawing", {
@@ -39,57 +40,61 @@ $(document).ready(function(){
                 location.replace("/welcome");
             }
         );
-    }); 
+    });
 
     var newUserGuess = '';
 
     $("#guess-submit").on("click", function (event) {
-    event.preventDefault();
-    newUserGuess = $("#guess").val().trim();
-    $("#rate-art").removeClass("hide");
-    $("#guess-form").addClass("hide");
+        event.preventDefault();
+        newUserGuess = $("#guess").val().trim();
+        $("#rate-art").removeClass("hide");
+        $("#guess-form").addClass("hide");
     });
 
     $("#submit-all").on("click", function (event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
-    //    var newUserGuess = $("#guess").val().trim();
-    var drawingID = $("#random-guessImg").data("attribute");
-    console.log(newUserGuess);   
-    console.log("drawing id: " + drawingID);
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
 
-    var userRating = parseInt($("#art-rating").val());
-    //new guess object
-    //var userScore = userRating * 10;
-    var userScore = {
-        user_score: userRating * 10,
-        id: 5
-    };
+        // Ensure values are used and numeric
+        var userRating = parseInt($("#art-rating").val());
+        var user_score = parseInt($("#user_score").val());
+        var artistID = parseInt($("#artistID").val());
+        var drawingID = parseInt($("#drawingID").val());
+        var guessUID = parseInt($("#guessUID").val());
 
-    var guessInput = {
-        guess: newUserGuess,
-        DrawingId: drawingID,
-        rating: userRating
-    };
-    
-    // Send the POST request.
-    $.ajax("/api/guess", {
-        type: "POST",
-        data: guessInput
-    }).then(
-        function () {
-            console.log("created new guess");
-        }
-    );
+        // update user object with new score
+        var userScore = user_score + (userRating * 10);
+        var userScore = {
+            user_score: userScore,
+            id: artistID
+        };
 
-    $.ajax("/api/score", {
-        type: "PUT",
-        data: userScore
-    }).then(
-        function () {
-            console.log("user score updated");
-            // Reload the page to get the updated list
-            location.replace("/welcome");
-        });
+        // new guess object
+        var guessInput = {
+            guess: newUserGuess,
+            DrawingId: drawingID,
+            rating: userRating,
+            userId: guessUID
+        };
+
+        // Send the POST request.
+        $.ajax("/api/guess", {
+            type: "POST",
+            data: guessInput
+        }).then(
+            function () {
+                console.log("created new guess");
+            }
+        );
+
+        $.ajax("/api/score", {
+            type: "PUT",
+            data: userScore
+        }).then(
+            function () {
+                console.log("user score updated");
+                // Reload the page to get the updated list
+                location.replace("/welcome");
+            });
     });
 });
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,40 +2,64 @@ var path = require("path");
 var db = require("../models");
 
 // routes
-module.exports = function(app) {
-  app.get("/", function(req, res) {
+module.exports = function (app) {
+  app.get("/", function (req, res) {
     res.render("index");
   });
 
-  app.get("/welcome", function(req, res) {
+  app.get("/welcome", function (req, res) {
     var userScore;
     var userName = req.user.user_name;
     db.User.findOne({
       where: {
         id: req.user.id
       }
-    }).then(function(user){
+    }).then(function (user) {
       userScore = user.user_score;
       console.log(userScore);
-      res.render("welcome", {username: userName, score: userScore});
+      res.render("welcome", { username: userName, score: userScore });
     });
   });
 
-  app.get("/draw", function(req, res) {
-    res.render("draw");
+  app.get("/draw", function (req, res) {
+    res.render("draw", { userID: req.user.id });
   });
 
-  app.get("/guess", function(req, res) {
-    db.Drawing.findOne({ 
-      exclude: {UserId: req.user.id}
-    }).then(function(drawing) {
-      // random drawing
-      var randomDrawing = drawing.dataValues.drawing;
-      var drawingDescriptor = drawing.dataValues.drawing_descriptor;
-      var drawingID = drawing.dataValues.id;
-      res.render("guess", {drawing: randomDrawing, answer: drawingDescriptor, id: drawingID});
-    });
+  // Find a drawing for guess
+  app.get("/guess", function (req, res) {
 
+    // find one drawing
+    db.Drawing.findOne({
+      attributes: ['UserId', 'drawing_descriptor', 'id', 'drawing'],
+      // test that the drawing's creator is not the user logged on
+      where: { userId: { [db.Sequelize.Op.ne]: req.user.id } }
+    }).then(function (drawingData) {
+      // var artistID = drawingData.dataValues.UserId;
+      // var descriptor = drawingData.dataValues.drawing_descriptor;
+      var drawingID = drawingData.dataValues.id;
+
+      // find user score for drawing found above
+      db.User.findOne({
+        attributes: [['id', 'drawingUID'], 'user_name', 'user_score'],
+        include: [{
+          model: db.Drawing,
+          where: { id: drawingID }
+        }]
+      }).then(function (userData) {
+        // format data to pass into html
+        var guessData = {
+          "guessUID": req.user.id,
+          "artistID": drawingData.dataValues.UserId,
+          "answer": drawingData.dataValues.drawing_descriptor,
+          "drawingID": drawingID,
+          "user_name": userData.dataValues.user_name,
+          "user_score": userData.dataValues.user_score,
+          "drawing": drawingData.dataValues.drawing
+        }
+        // res.json(results);
+        res.render("guess", guessData);
+      });
+    });
   });
 
 };
